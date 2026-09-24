021c4ec20497b1a0e76b253f5d64380b4142de9b
Roberto Benavides <robertobenavides@192.168.0.68>
2026-09-24 17:49:45 -0300

    karpenter

--- betodalas-terraform/infra/environments/prod/karpenter.tf
+++ betodalas-terraform/infra/environments/prod/karpenter.tf
@@ -1,3 +1,31 @@
+data "aws_iam_policy_document" "karpenter_controller_assume_role" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [module.eks.oidc_provider_arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(module.eks.cluster_oidc_issuer_url, "https://", "")}:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(module.eks.cluster_oidc_issuer_url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:karpenter:karpenter"]
+    }
+  }
+}
+
+resource "aws_iam_role" "karpenter_controller" {
+  name               = "${local.name}-karpenter-controller"
+  assume_role_policy = data.aws_iam_policy_document.karpenter_controller_assume_role.json
+}
+
 resource "aws_iam_role_policy" "karpenter_controller" {
   name = "${local.name}-karpenter-controller"
   role = aws_iam_role.karpenter_controller.id
@@ -23,40 +51,25 @@ resource "aws_iam_role_policy" "karpenter_controller" {
           "ec2:DescribeSubnets",
           "ec2:RunInstances",
           "ec2:TerminateInstances",
-          "ec2:DescribeInstanceStatus",
           "pricing:GetProducts"
         ]
         Resource = "*"
       },
       {
-        Sid      = "AllowInstanceProfileReadActions"
-        Effect   = "Allow"
-        Action   = "iam:GetInstanceProfile"
-        Resource = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:instance-profile/*"
-      },
-      {
-        Sid    = "AllowInstanceProfileMutationActions"
         Effect = "Allow"
         Action = [
           "iam:CreateInstanceProfile",
           "iam:AddRoleToInstanceProfile",
           "iam:RemoveRoleFromInstanceProfile",
           "iam:DeleteInstanceProfile",
-          "iam:TagInstanceProfile"
+          "iam:GetInstanceProfile",
+          "iam:TagInstanceProfile",
+          "iam:PassRole"
         ]
         Resource = [
-          "arn:aws:iam::${data.aws_caller_identity.current.account_id}:instance-profile/karpenter/${var.region}/${local.name}/*"
+          aws_iam_role.karpenter_node.arn,
+          "arn:aws:iam::${data.aws_caller_identity.current.account_id}:instance-profile/${local.name}-karpenter*"
         ]
-      },
-      {
-        Effect   = "Allow"
-        Action   = "iam:PassRole"
-        Resource = aws_iam_role.karpenter_node.arn
-      },
-      {
-        Effect   = "Allow"
-        Action   = "iam:ListInstanceProfiles"
-        Resource = "*"
       },
       {
         Effect   = "Allow"
@@ -70,4 +83,39 @@ resource "aws_iam_role_policy" "karpenter_controller" {
       }
     ]
   })
+}
+
+data "aws_iam_policy_document" "karpenter_node_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "karpenter_node" {
+  name               = "${local.name}-karpenter-node"
+  assume_role_policy = data.aws_iam_policy_document.karpenter_node_assume_role.json
+}
+
+locals {
+  karpenter_node_policies = {
+    worker = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+    cni    = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+    ecr    = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPullOnly"
+    ssm    = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "karpenter_node" {
+  for_each   = local.karpenter_node_policies
+  role       = aws_iam_role.karpenter_node.name
+  policy_arn = each.value
+}
+
+resource "aws_iam_instance_profile" "karpenter" {
+  name = "${local.name}-karpenter"
+  role = aws_iam_role.karpenter_node.name
 }
